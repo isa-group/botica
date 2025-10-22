47d52449236bd5d1192b52268326edda01a62c8f
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2025-10-22 19:07:07 +0200

    style: remove unused log property

--- botica-core/src/main/java/es/us/isa/botica/configuration/MainConfiguration.java
+++ botica-core/src/main/java/es/us/isa/botica/configuration/MainConfiguration.java
@@ -14,12 +14,8 @@ import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MainConfiguration implements Configuration {
-  private static final Logger log = LoggerFactory.getLogger(MainConfiguration.class);
-
   @JsonProperty("docker")
   private DockerConfiguration dockerConfiguration = new DockerConfiguration();
 
