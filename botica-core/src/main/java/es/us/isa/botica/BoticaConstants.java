e040949695da9fbdea69adcde596f50e43cffa20
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2025-11-10 13:30:17 +0100

    style: reformat BoticaConstants

--- botica-core/src/main/java/es/us/isa/botica/BoticaConstants.java
+++ botica-core/src/main/java/es/us/isa/botica/BoticaConstants.java
@@ -7,6 +7,5 @@ public final class BoticaConstants {
   public static final String BOT_TYPE_ENV = "BOTICA_BOT_TYPE";
   public static final String BOT_ID_ENV = "BOTICA_BOT_ID";
 
-  private BoticaConstants() {
-  }
+  private BoticaConstants() {}
 }
